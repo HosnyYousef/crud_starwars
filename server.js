f06fe16ac8ff24d4d047c0efa4efb691adda401e
Yousef Hosny <Yousefhosny.hm@gmail.com>
2025-01-08 13:32:29 +0200

    js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 // server.js
 console.log('May Node be with you')
 const express = require('express')
-const bodyParser = require('body-parser') 
+// const bodyParser = require('body-parser') 
 //bodyParser is deprecated, we can take it out later
 const app = express()
 
 
 //bodyParser is deprecated, we can take it out later
-app.use(bodyParser.urlencoded({extended: true}))
+// app.use(bodyParser.urlencoded({extended: true}))
+
+
+// Make sure you place this before your CRUD handlers!
+app.use(express.urlencoded({ extended: true }))
+
 
 app.listen(3000, function () {
     console.log('listening on 3000')
@@ -16,5 +21,7 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
 })
 app.post('/quotes', (req, res) => {
-    console.log("hellllooooooooo")
+    console.log(req.body)
 })
+
+
