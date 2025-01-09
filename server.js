28721fab08e241240baf9ff50a86b2985d0c5f1d
Yousef Hosny <Yousefhosny.hm@gmail.com>
2025-01-09 12:06:00 +0200

    mongoDB ver 2. Got the data base to connect

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const express = require('express')
 //bodyParser is deprecated, we can take it out later
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-const connectionString = 'mongodb://test:testtesttest@ac-knf3crk-shard-00-00.m4qnvex.mongodb.net:27017,ac-knf3crk-shard-00-01.m4qnvex.mongodb.net:27017,ac-knf3crk-shard-00-02.m4qnvex.mongodb.net:27017/?ssl=true&replicaSet=atlas-it8cwt-shard-0&authSource=admin&retryWrites=true&w=majority'
+const connectionString = 'mongodb://user2000:test123@ac-knf3crk-shard-00-00.m4qnvex.mongodb.net:27017,ac-knf3crk-shard-00-01.m4qnvex.mongodb.net:27017,ac-knf3crk-shard-00-02.m4qnvex.mongodb.net:27017/?ssl=true&replicaSet=atlas-it8cwt-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
 
+//  'mongodb+srv://user2000:test123@cluster0.m4qnvex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
-// 'mongodb+srv://test:testtesttest@cluster0.m4qnvex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+// 'mongodb://test:testtesttest@ac-knf3crk-shard-00-00.m4qnvex.mongodb.net:27017,ac-knf3crk-shard-00-01.m4qnvex.mongodb.net:27017,ac-knf3crk-shard-00-02.m4qnvex.mongodb.net:27017/?ssl=true&replicaSet=atlas-it8cwt-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 
 //bodyParser is deprecated, we can take it out later
@@ -23,8 +24,8 @@ MongoClient.connect(connectionString, (err, client) => {
 app.use(express.urlencoded({ extended: true }))
 
 
-app.listen(3000, function () {
-    console.log('listening on 3000')
+app.listen(9000, function () {
+    console.log('listening on 9000')
   })
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
